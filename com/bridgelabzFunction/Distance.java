3f5ee034c04b40757b612fc1e695b7c1923471f3
Shital <114717834+ShitalKarad@users.noreply.github.com>
2022-11-22 19:56:54 +0530

    Update Distance.java

--- com/bridgelabzFunction/Distance.java
+++ com/bridgelabzFunction/Distance.java
@@ -2,9 +2,11 @@ package com.bridgelabzFunction;
 
 public class Distance {
     public static void main(String[] args) {
-
-        int x = Integer.valueOf(args[0]);
+        int x =  Integer.valueOf(args[0]);
         int y = Integer.valueOf(args[1]);
+        distance(x , y);    }
+
+    public  static void distance(int x, int y){
         int first = x*x;
         int second = y*y;
         double sum = first + second;
